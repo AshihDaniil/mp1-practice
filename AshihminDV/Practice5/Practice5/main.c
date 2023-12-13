58bab4d6f40c88be5a7fe646ad6498e485776b9e
AshihDaniil <danyaomni7b@mail.ru>
2023-12-13 13:10:20 +0300

    int to str shtrih

--- AshihminDV/Practice5/Practice5/main.c
+++ AshihminDV/Practice5/Practice5/main.c
@@ -4,49 +4,44 @@
 #include <string.h>
 #include <locale.h>
 #define N 6
+#define MAX_LEN 254
 
 char* baza[N] = { "1111", "1201","1301", "1401", "2402","1503" };
 char* names[N] = { "Хлеб", "Вода","Молоко", "Пельмени", "Макароны", "Шоколад" };
-int price[N] = { 30, 40, 70, 150, 95, 80 };
+double price[N] = { 30.0, 40.0, 70.0, 150.0, 95.0, 80.0 };
 double sale[N];
 
 void make_sale(int n);
 void tovar_info(int i, int k);
-int chek_shtrih(int n, int shtrih);
+int chek_shtrih(int n, char *shtrih);
+void chek(int *kt, double *sum, double *r_skidka);
 
 int main() {
-	setlocale(LC_ALL, "Rus");
-	int shtih, i, sum=0;
-	char shtrih = 0;
-	double r_skidka = 0.0;
+	int i;
+	char shtih[MAX_LEN];
+	double r_skidka = 0.0, sum = 0.0;
 	int kt[N] = { 0, 0, 0, 0, 0, 0 };
-	printf("Введите штрихкод товара, для завешения покупки введите 0\n");
+	setlocale(LC_ALL, "Rus");
+	printf("Введите штрихкод товара, для завешения покупки введите 'res'\n");
 	make_sale(N);
 	do {
-		scanf(" %d", &shtih);
-		if (shtih != 0) {
-			i = chek_shtrih(N, shtih);
-			while (i == -1) {
-				printf("Неверный штрихкод\n");
-				scanf(" %d", &shtih);
-				i = chek_shtrih(N, shtih);
-			}
-			tovar_info(i, 0);
-			kt[i]++;
+		scanf("%s", &shtih);
+		i = chek_shtrih(N, &shtih[0]);
+		if (i == -1) {
+			printf("Неверный штрихкод\n");
+			continue;
 		}
-	} while (shtih != 0);
+		if(i!=-2){
+			tovar_info(i, 0);
+			kt[i]++;	
+		}
+	} while (strcmp(shtih, "res")!=0);
 	printf("\n");
 	printf("\nВаш чек\n");
-	for (i = 0; i < N; i++) {
-		sum += kt[i] * price[i];
-		r_skidka += price[i] * sale[i] * kt[i];
-		if (kt[i] != 0) {
-			tovar_info(i, kt[i]);
-		}
-	}
-	printf("\nЦена без скидки составляет %d\n", sum);
+	chek(&kt[0], &sum, &r_skidka);
+	printf("\nЦена без скидки составляет %.2lf\n", sum);
 	printf("Скидка составляет %.2lf\n", r_skidka);
-	printf("К оплате: %.2lf\n", (double)sum - r_skidka);
+	printf("К оплате: %.2lf\n", sum - r_skidka);
 }
 void make_sale(int n) {
 	int i;
@@ -57,21 +52,34 @@ void make_sale(int n) {
 }
 void tovar_info(int i, int k) {
 	printf("Наименование товара %s\n", names[i]);
-	printf("Цена %d\n", price[i]);
+	printf("Цена %.2lf\n", price[i]);
 	printf("Скидка %.0lf процентов\n", sale[i]*100);
 	if (k != 0) {
 		printf("Количество: %d\n", k);
 	}
 }
 
-int chek_shtrih(int n, int shtrih) {
-	int i = 0, trash = shtrih;
-	char str_shtrih[5];
-	_itoa(trash, str_shtrih, 10);
-	for (i = 0; i < n; i++) {
-		if (strcmp(str_shtrih, baza[i]) == 0){
+int chek_shtrih(int n, char *shtrih) {
+	int i;
+	if (strcmp(shtrih, "res") == 0) {
+		printf("Завершение покупки\n");
+		return -2;
+	}
+	for (i=0; i < n; i++) {
+		if (strcmp(shtrih, baza[i])==0) {
 			return i;
 		}
 	}
 	return -1;
+}
+
+void chek(int* kt, double* sum, double* r_skidka) {
+	int i = 0;
+	for (i = 0; i < N; i++) {
+		*sum += (double)(kt[i] * price[i]);
+		*r_skidka += price[i] * sale[i] * kt[i];
+		if (kt[i] != 0) {
+			tovar_info(i, kt[i]);
+		}
+	}
 }
