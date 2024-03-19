210cbb893afdeb353bc272468f71ffca3e764333
AshihDaniil <danyaomni7b@mail.ru>
2024-03-19 20:12:32 +0300

    2

--- AshihminDV/PracticevectorCPP/PracticevectorCPP/main.cpp
+++ AshihminDV/PracticevectorCPP/PracticevectorCPP/main.cpp
@@ -1,23 +1,27 @@
 #include "vector.h"
-int main() 
+int main(int argc, char **argv)
 {
+	if(argc<3)
+	{
+		std::cout << "Incorrect argument" << std::endl;
+		return 1;
+	}
+	char* infilename = argv[1], * outfilename = argv[2];
+	std::ofstream out;
+	std::ifstream in;
 	TVector v1, v2;
-	std::cin >> v1 >> v2;
+
+	in.open(infilename);
+	in >> v1 >> v2;
+	in.close();
+
 	TVector res_sum = v1 + v2;
-	std::cout << "Summa vectorov"<< std::endl;
-	std::cout << v1;
-	std::cout << v2;
-	std::cout << res_sum;
 	TVector res_razn = v1 - v2;
-	std::cout << "Razn vectorov" << std::endl;
-	std::cout << v1;
-	std::cout << v2;
-	std::cout << res_razn;
 	double skalpr = v1 * v2;
-	std::cout << "skalyarnoe proizvedenie vectorov" << std::endl;
-	std::cout << v1;
-	std::cout << v2;
-	std::cout << skalpr << std::endl;
+
+	out.open(outfilename);
+	out << res_sum << res_razn << "skalpr = " << skalpr << std::endl;
+	out.close();
 
 	return 0;
 }
--- AshihminDV/PracticevectorCPP/PracticevectorCPP/vector.h
+++ AshihminDV/PracticevectorCPP/PracticevectorCPP/vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 
 struct TVector
 {
@@ -14,6 +15,30 @@ struct TVector
 	TVector operator- (const TVector&);
 	double operator* (const TVector&);
 	TVector& operator= (const TVector&);
+	friend std::ofstream& operator<<(std::ofstream& out, const TVector& V)
+	{
+		out << "Vector: ";
+		for (int i = 0; i < V.n; i++)
+		{
+			out << V.x[i] << " ";
+		}
+		out << std::endl;
+		return out;
+	}
+	friend std::ifstream& operator<<(std::ifstream& in, TVector& V)
+	{
+		in >> V.n;
+		if (V.x != nullptr)
+		{
+			delete[] V.x;
+		}
+		V.x = new double[V.n];
+		for (int i = 0; i < V.n; i++)
+		{
+			in >> V.x[i];
+		}
+		return in;
+	}
 };
 
 std::istream& operator>>(std::istream& in, TVector& v);
